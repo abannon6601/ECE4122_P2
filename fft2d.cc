22e3769c8bc1b49805b372612bd5e3a260926079
abannon6601 <a.bannon6601@gmail.com>
2018-03-13 09:03:36 -0400

    Final code

--- fft2d.cc
+++ fft2d.cc
@@ -31,7 +31,7 @@ using namespace std;
 //graduate students should assume NUMTHREADS will not always evenly divide the number of rows in tested images.
 //I will test with a different image than the one given
 
-void Transform2D(const char *inputFN, int inverse)
+void Transform2D(const char *inputFN)
 { // Do the 2D transform here.
   // 1) Use the InputImage object to read in the Tower.txt file and
   //    find the width/height of the input image.
@@ -48,16 +48,14 @@ void Transform2D(const char *inputFN, int inverse)
 
     Complex *H = new Complex[image.GetWidth() * image.GetHeight()]; // Allocate the results array
 
-    Complex *h = image.GetImageData();
+    Complex *h = image.GetImageData();  // create a local pointer to the raw data
 
     std::vector<std::thread> vectors;
 
     for(int i = 0; i < NUMTHREADS; ++i)
     {
-        vectors.push_back(std::thread(Transform1D, h, image.GetWidth(), H, i, inverse)); // fire vectors at the problem
-    //    std::cout << "FFT2D_DEBUG: Running thread: " << i << std::endl;
+        vectors.push_back(std::thread(Transform1D, h, image.GetWidth(), H, i, -1)); // fire vectors at the problem
     }
-    //std::cout << "FFT2D_DEBUG: Running " << vectors.size() << " threads" << std::endl;
 
     std::for_each(vectors.begin(),vectors.end(),do_join);   // ensure all threads are complete before continuing
     vectors.clear();
@@ -73,7 +71,7 @@ void Transform2D(const char *inputFN, int inverse)
 
      for(int i = 0; i < NUMTHREADS; ++i)
      {
-         vectors.push_back(std::thread(Transform1D, h, image.GetWidth(), H, i, inverse)); // fire vectors at the problem
+         vectors.push_back(std::thread(Transform1D, h, image.GetWidth(), H, i, -1)); // fire vectors at the problem
      }
 
      std::for_each(vectors.begin(),vectors.end(),do_join);   // ensure all threads are complete before continuing
@@ -84,7 +82,52 @@ void Transform2D(const char *inputFN, int inverse)
      Transpose(H, image.GetWidth());  // transpose the result matrix
 
     string filename = "MyAfter2D.txt";
+    image.SaveImageData(filename.c_str(),H,image.GetWidth(),image.GetHeight());
 
+
+
+
+    // now do inverse transform
+
+
+    for(int i = 0; i < image.GetWidth()*image.GetHeight(); ++i) // copy the result matrix back into the orginal array
+    {
+        h[i] = H[i];
+    }
+
+    for(int i = 0; i < NUMTHREADS; ++i)
+    {
+        vectors.push_back(std::thread(Transform1D, h, image.GetWidth(), H, i, 1)); // fire vectors at the problem
+        //    std::cout << "FFT2D_DEBUG: Running thread: " << i << std::endl;
+    }
+
+    std::for_each(vectors.begin(),vectors.end(),do_join);   // ensure all threads are complete before continuing
+    vectors.clear();
+
+    Transpose(H, image.GetWidth());  // transpose the result matrix
+
+    for(int i = 0; i < image.GetWidth()*image.GetHeight(); ++i) // copy the result matrix back into the orginal array
+    {
+        h[i] = H[i];
+    }
+
+    for(int i = 0; i < NUMTHREADS; ++i)
+    {
+        vectors.push_back(std::thread(Transform1D, h, image.GetWidth(), H, i, 1)); // fire vectors at the problem
+    }
+
+    std::for_each(vectors.begin(),vectors.end(),do_join);   // ensure all threads are complete before continuing
+    vectors.clear();
+
+    Transpose(H, image.GetWidth());  // transpose the result matrix
+
+    for(int i = 0; i < image.GetWidth()*image.GetHeight(); ++i) // remove the small errors introduced during the DFT process to recover the original image
+    {
+        H[i].imag = 0;
+        H[i].real = round(H[i].real);
+    }
+
+    filename = "MyAfterInverse.txt";
     image.SaveImageData(filename.c_str(),H,image.GetWidth(),image.GetHeight());
 }
 
@@ -107,9 +150,10 @@ void Transpose(Complex *input, int dim) // transposes the square matrix  *input 
     }
 
     delete(HoldingInput);
+    // there are faster and more efficent ways to do this, but this is easier to follow and code
 }
 
-void do_join(std::thread& t)
+void do_join(std::thread& t)    // simple function called to join a thread
 {
     t.join();
 }
@@ -122,9 +166,8 @@ void Transform1D(Complex *h, int w, Complex *H, int numberInSequence, int invers
     // nunmberInSequence tells the funciton how many calls have already been made, and so where it should
     // start in the h array.
 
-    Complex j = Complex(0,1);
     Complex W;
-    W.imag = inverse * (sin((2*M_PI)/w));
+    W.imag = inverse * (sin((2*M_PI)/w));   //setup constants. "inverse" here is used if this is an IDFT
     W.real = cos((2*M_PI)/w);
 
     int startPoint   = ((w*w)/NUMTHREADS)*numberInSequence;  // where each thread will start working in the array
@@ -134,7 +177,7 @@ void Transform1D(Complex *h, int w, Complex *H, int numberInSequence, int invers
     int rowStartIndex = 0;
 
     Complex sum_holdder(1,0);
-    Complex w_n(1,0);
+    Complex w_n(1,0);           // utitilty objects
     Complex w_nk(1,0);
 
     for(int i = 0; i < numberOfRows; i++)
@@ -150,20 +193,27 @@ void Transform1D(Complex *h, int w, Complex *H, int numberInSequence, int invers
 
             sum_holdder.real = 0;
             sum_holdder.imag = 0;
+            w_nk.real = 1;
+            w_nk.imag = 0;
 
-            w_n.real = 1;
-            w_n.imag = 0;
-
-
-            for(int k = 0; k < (w-1); k++)
+            for(int k = 0; k < w; k++)
             {
-                sum_holdder = sum_holdder + w_nk * h[rowStartIndex + k];
+                sum_holdder = sum_holdder + w_nk * h[rowStartIndex + k];    //perform each step of the DFT
                 w_nk = w_nk*w_n;
             }
-            H[workingIndex].real = sum_holdder.real;
-            H[workingIndex].imag = sum_holdder.imag;
+            if(inverse == -1)
+            {
+                H[workingIndex].real = sum_holdder.real;    //DFT
+                H[workingIndex].imag = sum_holdder.imag;
+            }
+            else
+            {
+                H[workingIndex].real = sum_holdder.real/w;  // inverse DFT
+                H[workingIndex].imag = sum_holdder.imag/w;
+            }
 
-            w_n = w_n * W;
+
+            w_n = w_n * W;  // incriment the power of w_n
         }
     }
 
@@ -174,8 +224,7 @@ int main(int argc, char** argv)
   string fn("Tower.txt"); // default file name
   if (argc > 1) fn = string(argv[1]);  // if name specified on cmd line
 
-    Transform2D(fn.c_str(), -1); // Perform the transform.
-    // Transform2D(fn.c_str(), 1); // Perform the inverse transform.
+    Transform2D(fn.c_str()); // Perform the transform.
 
 }
   
